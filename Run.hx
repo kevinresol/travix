7c1712c93043a49cc9db1d736feb07024f0a47ff
Kevin Leung <kevinresol@gmail.com>
2026-07-25 22:08:47 +1000

    Try haxelib.main again

--- Run.hx
+++
@@ -1,6 +0,0 @@
-class Run {
-	static function main() {
-		final code = Sys.command('haxe -lib tink_cli -lib hx3compat -cp src -D no-deprecation-warnings --run travix.Travix ${Sys.args().map(v -> '"$v"').join(' ')}');
-		Sys.exit(code);
-	}
-}
